f1257756156531e4a5da84941992c524775441d9
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-23 05:31:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|x, y| -(x <=> y)}
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+
+end
+
+def find_a(array)
+  array.start_with?("a")
+end
+
+def sum_array(array)
+  array.inject(0){|sum, x| sum + x}
+end
+
+def add_s(array)
+
+end
